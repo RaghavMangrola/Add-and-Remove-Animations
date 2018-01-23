1c86cbb95c1e9d470f91f184b5e9a0738742bb09
Raghav Mangrola <raghav.mangrola@eventfarm.com>
2018-01-23 11:17:23 -0800

    Initial Commit

---
+++ Animation Pause and Resume/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animation Pause and Resume
+//
+//  Created by Raghav Mangrola on 1/23/18.
+//  Copyright © 2018 Raghav Mangrola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
